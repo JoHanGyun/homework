c75f64d61f6ff2a17f55467aa6373422f98ff612
JoHanGyun <109188612+JoHanGyun@users.noreply.github.com>
2023-03-21 22:37:00 +0900

    03.21

---
+++ 03.21.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+//	int h, m;
+//
+//    scanf("%d:%d", &h, &m);
+//    printf("%d:%d", h, m);
+
+//	int year, month, day;
+//	
+//	scanf("%d.%d.%d",&year,&month,&day);
+//	printf("%d.%2d,%2d",year,month,day);
+
+
+}
